4b3e34a9ce84b425e5e5c9e289fe514a0ec38b9a
Dan Kirkwood <dangogh@gmail.com>
2014-02-02 21:50:59 -0700

    more experiment

--- jsonmerge/jsonmerge.go
+++ jsonmerge/jsonmerge.go
@@ -2,22 +2,58 @@ package jsonmerge
 
 import (
 	//"encoding/json"
+	"errors"
 	"fmt"
 	//"io"
 )
 
-type Merger map[string]interface{}
+type Merger interface {
+	Merge(interface{}) error
+}
 
-func (r Merger) Merge(a Merger) error {
-	for k, v := range a {
+type Accum struct {
+	val interface{}
+}
+
+/*
+	A	B	RES
+	*	null	A
+	null	*	B
+	slice	slice	A+B
+	slice	map	A
+	map	slice	A
+	map	map	A+B
+
+*/
+
+func (accum *Accum) mergeSlice(tomerge []interface{}) error {
+	if accum.val == nil {
+		accum.val = tomerge
+		return nil
+	}
+	if a, ok := accum.val.([]interface{}); ok {
+		accum.val = append(a, tomerge)
+		return nil
+	}
+	return errors.New(fmt.Sprintf("Can't merge %T with %T", accum.val, tomerge))
+}
+
+func (accum *Accum) mergeMap(tomerge map[interface{}]interface{}) error {
+	if a, ok := accum.(map[interface{}]interface{}); !ok {
+		return errors.NewError
+	for k, v := range accum.val {
 		switch v.(type) {
 		case []interface{}:
-			fmt.Printf("%v - slice %T\n", k, v)
+			accum.val[k].mergeSlice(v)
 		case map[interface{}]interface{}:
-			fmt.Printf("%v - map %T\n", k, v)
+			accum.val[k].mergeMap(v)
 		default:
-			r[k] = v
+			accum.val[k] = v
 		}
 	}
 	return nil
+}
+
+func (accum *Accum) Merge(tomerge interface{}) error {
+	
 }
--- jsonmerge/jsonmerge_test.go
+++ jsonmerge/jsonmerge_test.go
@@ -7,11 +7,13 @@ import (
 )
 
 func TestMergeOneLevel(t *testing.T) {
-	m1 := Merger{"X": 1}
-	t1 := Merger{"Y": 2}
-	m2 := Merger{"X": 1, "Y": 2}
-	m1.Merge(t1)
-	eq := reflect.DeepEqual(m1, m2)
+	x1 := map[string]int{"X": 1}
+	y1 := map[string]int{"Y": 2}
+	var m1 Merger
+	m1.Merge(x1)
+	m1.Merge(y1)
+	var res1 map[string]int = m1
+	eq := reflect.DeepEqual(x1, res1)
 	if !eq {
 		t.Errorf("first level failed")
 	}
